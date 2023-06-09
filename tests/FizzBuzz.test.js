772a25742f0f018fdf9ad771894c0911185e21df
DevBrianAcostaBenitez <brianacostabenitez@gmail.com>
2023-06-09 09:22:22 +0100

    changed expected to be to assert.equal

--- tests/FizzBuzz.test.js
+++ tests/FizzBuzz.test.js
@@ -7,31 +7,31 @@ describe('FizzBuzz', () => {
         const numb=3 
         const fizzBuzz = new FizzBuzz()
         const result = fizzBuzz.check(numb);
-        expect(result).toBe("Fizz")
+        assert.equal(result,"Fizz")
     });
     it('if number divisible by 5 return Buzz', () => {
         const numb=5
         const fizzBuzz = new FizzBuzz()
         const result = fizzBuzz.check(numb);
-        expect(result).toBe("Buzz")
+        assert.equal(result, "Buzz")
     });
 
     it('if number divisible by 5 and 3 return FizzBuzz', () => {
         const numb=15
         const fizzBuzz = new FizzBuzz()
         const result = fizzBuzz.check(numb);
-        expect(result).toBe("FizzBuzz")
+        assert.equal(result,"FizzBuzz")
     });
     it('if number has 3 return Fizz', () => {
         const numb=13
         const fizzBuzz = new FizzBuzz()
         const result = fizzBuzz.check(numb);
-        expect(result).toBe("Fizz")
+        assert.equal(result,"Fizz")
     });
     it('if number has 5 return buzz', () => {
         const numb=25
         const fizzBuzz = new FizzBuzz()
         const result = fizzBuzz.check(numb);
-        expect(result).toBe("Buzz")
+        assert.equal(result,"Buzz")
     });
 });
