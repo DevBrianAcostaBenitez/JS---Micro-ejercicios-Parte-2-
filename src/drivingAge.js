b2309d6fa5aca9ad09c2f8f86c9d943f8aa3557d
DevBrianAcostaBenitez <brianacostabenitez@gmail.com>
2023-06-17 14:23:53 +0100

    more indent

--- src/drivingAge.js
+++ src/drivingAge.js
@@ -2,14 +2,14 @@ export class DrivingAge {
     check(person) {
       const isTestEnvironment = typeof window === "undefined";
       if (person.age >= 18) {
-          if (isTestEnvironment) {
-        return person.name + " is "+ person.age + " and can drive";
+        if (isTestEnvironment) {
+          return person.name + " is "+ person.age + " and can drive";
         }else{
           alert (person.name + " is "+ person.age + " and can drive");
         }
       } else {
-          if (isTestEnvironment) {
-        return person.name + " is "+ person.age + " and can\'t drive";
+        if (isTestEnvironment) {
+          return person.name + " is "+ person.age + " and can\'t drive";
         }else{
           alert (person.name + " is "+ person.age + " and can\'t drive");
         }
